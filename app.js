73662e6400eaa67833fa27a43724391b5d384308
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-10-03 19:59:11 +0100

    add Set conversion - logs moves taken in array with move count

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 
 const Node = (pos, prevPos, visited) => {
-// console.log("-----NODE-----")
 
-   // console.log("position: ", pos )
-   // console.log("visited: ", visited )
-    // console.log("---------------")
     const neighbours = []
     const position = pos
-
     const numAsString = `${pos[0]}${pos[1]}`
        
-    let visitedSquares = new Set(visited)
+    let visitedSquares = new Set(visited) 
     visitedSquares.add(numAsString)
-    visitedSquares.delete(0)
-   
-    // let visitedSquares = [pos, ...visited]  //   const visitedSquares = [pos, ...visited]
 
-    // if any value in the Set matches a proposed move skip
-        
+        // if any value in the Set matches a proposed move skip
         const a = `${pos[0]+ 1}${pos[1]+ 2}`
         const b = `${pos[0]+ 1}${pos[1]- 2}`
         const c = `${pos[0]+ 2}${pos[1]+ 1}`
@@ -27,67 +18,60 @@ const Node = (pos, prevPos, visited) => {
         const g = `${pos[0]- 2}${pos[1]+ 1}`
         const h = `${pos[0]- 1}${pos[1]+ 2}`
 
-        // console.log(a, b, c, d,e, f, g, h)
-        // console.log(visitedSquares, numAsString)
- 
-    // const getVisited = () => {
-    //     return true
-    // };
-    //  HORIZONTAL - VERTICAL
-    //  ROW  ---- COLUMN
+    //  ROW  ---- COLUMN // HORIZONTAL - VERTICAL
     const getNeighbours = () => {
  
         if ( (pos[0] + 1 <= 7 && pos[1] + 2 <= 7) && !visitedSquares.has(a)   ) {     // one right, two up
-            console.log( "one right, two up" )
+            
             visitedSquares.add(numAsString)
             visitedSquares.add(...visited)
             let neighbour = Node( [pos[0] + 1, pos[1] + 2], prevPos, visitedSquares )
             neighbours.push(neighbour)
         
         }  if ( (pos[0] + 1 <= 7 && pos[1] - 2 >= 0)  && !visitedSquares.has(b)  ) { // one right, two down
-            console.log( "one right, two down" )
+            
             visitedSquares.add(numAsString)
             visitedSquares.add(...visited)
             let neighbour = Node( [pos[0] + 1, pos[1] - 2], prevPos, visitedSquares )
             neighbours.push(neighbour)
 
         }  if ( (pos[0] + 2 <= 7 && pos[1] + 1 <= 7)  && !visitedSquares.has(c)  ) { // two right, one up
-            console.log( "two right, one up" )
+            
             visitedSquares.add(numAsString)
             visitedSquares.add(...visited)
             let neighbour = Node( [pos[0] + 2, pos[1] + 1], prevPos, visitedSquares )
             neighbours.push(neighbour)
         
         }  if ( (pos[0] + 2 <= 7 && pos[1] - 1 >= 0)  && !visitedSquares.has(d)  ) { // two right, one down
-            console.log( "two right, one down" )
+            
             visitedSquares.add(numAsString)
             visitedSquares.add(...visited)
             let neighbour = Node( [pos[0] + 2, pos[1] - 1], prevPos, visitedSquares )
             neighbours.push(neighbour)
 
         }  if ( (pos[0] - 2 >= 0 && pos[1] - 1 >= 0)  && !visitedSquares.has(e)  ) { // two left, one down
-            console.log( "two left, one up" )
+            
             visitedSquares.add(numAsString)
             visitedSquares.add(...visited)
             let neighbour = Node( [pos[0] - 2, pos[1] - 1], prevPos, visitedSquares )
             neighbours.push(neighbour)
 
         }  if ( (pos[0] - 1 >= 0 && pos[1] - 2 >= 0)  && !visitedSquares.has(f)  ) { // one left, two down
-            console.log( "one left, two up" )
+            
             visitedSquares.add(numAsString)
             visitedSquares.add(...visited)
             let neighbour = Node( [pos[0] - 1, pos[1] - 2], prevPos, visitedSquares )
             neighbours.push(neighbour)
 
-        }  if ( (pos[0] - 2 >= 0 && pos[1] + 1 <= 7)  && !visitedSquares.has(g)  ) { // two left, one down
-            console.log( "two left, one down" )
+        }  if ( (pos[0] - 2 >= 0 && pos[1] + 1 <= 7)  && !visitedSquares.has(g)  ) { // two left, one up
+            
             visitedSquares.add(numAsString)
             visitedSquares.add(...visited)
             let neighbour = Node( [pos[0] - 2, pos[1] + 1], prevPos, visitedSquares )
             neighbours.push(neighbour)
 
-        }  if ( (pos[0] - 1 >= 0 && pos[1] + 2 <= 7) && !visitedSquares.has(h)  ) { // one left, two down
-            console.log( "one left, two down" )
+        }  if ( (pos[0] - 1 >= 0 && pos[1] + 2 <= 7) && !visitedSquares.has(h)  ) { // one left, two up
+            
             visitedSquares.add(numAsString)
             visitedSquares.add(...visited)
             let neighbour = Node( [pos[0] - 1, pos[1] + 2], prevPos, visitedSquares )
@@ -103,78 +87,85 @@ const Node = (pos, prevPos, visited) => {
 const Tree = (start, previous = start, visited = start) => {
 
     let root = [];
-    root.push(buildTree(start, previous, visited));
 
+    const buildTree = (start, prevPos, visitedSquares) => {
+        
+        let root = Node(start, prevPos, visitedSquares)
+        root.getNeighbours()
+    
+        return root
+    };
+
+    root.push(buildTree(start, previous, visited));
 
     const search = (end, index) => {
 
         for (const prop of root[index].neighbours) {
 
-            // console.log(prop.position)
-            // console.log(index)
-
             if ( prop.position[0] === end[0] && prop.position[1] === end[1] ) {
-                console.log(root[index])
-                console.log(prop.position[0], prop.position[1], end)
-                console.log("match")
-                return end
+                return root[index].visitedSquares
             } 
         }
         return index
     };
 
-// this should receive the correct root[index] from search, i think
+    // this should receive the correct root[index] from search
     const insert = (previousNode) => {
-        console.log(previousNode)
 
         const neighbours = previousNode.neighbours
 
         neighbours.forEach(neighbour => {
             // calculate previous position
-            // console.log("-----INSERT-----")
-            // console.log(neighbour.position)
-            // console.log(previousNode.position)
-            // console.log(previousNode.visitedSquares)
-            // console.log("--------------")
             root.push(buildTree(neighbour.position, previousNode.position, previousNode.visitedSquares ))
         });     
     };
 
-    getRoot = () => root
+    const levelOrder = (tree, end, node = 0) => {
 
-    return { root, insert, search, getRoot }
+        let result = tree.search(end, node)
+    
+        if (result instanceof Set) return convertSet(result, end)
+
+        tree.insert(tree.root[result])
+        node++
+        // if search not successful then build next part of the tree
+        levelOrder(tree, end, node)   
+    };
+
+    return { root, insert, search, buildTree, levelOrder }
+};
+
+const convertSet = (result, end) => {
+
+    const path = []
+
+    const removeNumbers = (result) => {
+        result.forEach(item => {    
+            if (item < 10) {
+                result.delete(item)
+            }
+        });
+        return result
+    };
+
+    const convertToNumbers = (set) => {
+        
+        set.forEach(ele => {
+            path.push( [Number(ele.charAt(0)), Number(ele.charAt(1))] )
+        });
+    };
+
+    const revisedSet = removeNumbers(result, end)
+    convertToNumbers(revisedSet)
+    path.push(end)
+
+    console.log("shortest path: ", path)
+    console.log("steps taken: ", path.length - 1)
 };
 
 
-function knight(array = [2,2], end = [5,6]) {
+function knight(array = [1,2], end = [5,5]) {
 
     let tree = Tree(array)
-    console.log(tree)
-
-    next(tree, end) 
-};
-
-
-function next(tree, end, test = 0) {
-  //  console.log(test)
-    let index = tree.search(end, test)
-// console.log(index)
-    if (Array.isArray(index)) {
-      console.log(index)
-      return
-    } 
- 
-    tree.insert(tree.root[index])
-    test++
-    next(tree, end, test)
-    // if search not successful then build next part of the tree
-};
-
-
-function buildTree(start, prevPos, visitedSquares) {
-
-    let root = Node(start, prevPos, visitedSquares)
-    root.getNeighbours()
-
-    return root
+    tree.levelOrder(tree, end) 
 };
