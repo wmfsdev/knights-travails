f44bcd2ef9ba78ef4c8283e187b777e7ac26e50c
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-10-03 21:48:23 +0100

    minor tweak

--- app.js
+++ app.js
@@ -178,13 +178,14 @@ function knight(array = [1,2], end = [5,5]) {
 
 // DEMONSTRATION
 
+console.log("KNIGHTS TRAVAILS")
 console.log("    ________________________ ")
 console.log("7  |                       | ")
-console.log("6  |                       | ")
-console.log("5  |                       | ")
-console.log("4  |                       | ")
+console.log("6  |         2             | ")
+console.log("5  |               3       | ")
+console.log("4  |      1                | ")
 console.log("3  |                       | ")
-console.log("2  |                       | ")
+console.log("2  |    0                  | ")
 console.log("1  |                       | ")
 console.log("0  |_______________________| ")
 console.log("     0  1  2  3  4  5  6  7")
